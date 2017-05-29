a4c6273fec282a6618b6452c38025c767fdffb10
pepesan <pepesan@gmail.com>
2017-05-29 02:35:23 +0200

    modificadas rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: 'todo', component: TodoComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home ', pathMatch: 'full' },
+ // { path: 'manage-book', component: ManageBookComponent },
+ // { path: 'update-book/:id', component: UpdateBookComponent },
+ // { path: '', redirectTo: '/manage-book ', pathMatch: 'full' },
+ // { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
